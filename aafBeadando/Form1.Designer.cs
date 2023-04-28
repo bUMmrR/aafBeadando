5166bc5fd329d01b4be8ced71ce7aebd9a5c9468
bUMmrR <agocsdaniel5454@gmail.com>
2023-04-28 15:15:59 +0200

    kész feladat

---
+++ aafBeadando/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace aafBeadando
+{
+    partial class listak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.lb1 = new System.Windows.Forms.ListBox();
+            this.lb2 = new System.Windows.Forms.ListBox();
+            this.hozzaad1 = new System.Windows.Forms.Button();
+            this.hozzaad2 = new System.Windows.Forms.Button();
+            this.felulre1 = new System.Windows.Forms.Button();
+            this.fel1 = new System.Windows.Forms.Button();
+            this.le1 = new System.Windows.Forms.Button();
+            this.alulra1 = new System.Windows.Forms.Button();
+            this.alulra2 = new System.Windows.Forms.Button();
+            this.le2 = new System.Windows.Forms.Button();
+            this.fel2 = new System.Windows.Forms.Button();
+            this.felulre2 = new System.Windows.Forms.Button();
+            this.balraKuld = new System.Windows.Forms.Button();
+            this.jobraKuld = new System.Windows.Forms.Button();
+            this.torol1 = new System.Windows.Forms.Button();
+            this.torol2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(34, 32);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(182, 20);
+            this.text1.TabIndex = 0;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(419, 32);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(182, 20);
+            this.text2.TabIndex = 1;
+            // 
+            // lb1
+            // 
+            this.lb1.FormattingEnabled = true;
+            this.lb1.Location = new System.Drawing.Point(34, 64);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(182, 342);
+            this.lb1.TabIndex = 2;
+            this.lb1.SelectedIndexChanged += new System.EventHandler(this.lb1_SelectedIndexChanged);
+            // 
+            // lb2
+            // 
+            this.lb2.FormattingEnabled = true;
+            this.lb2.Location = new System.Drawing.Point(419, 64);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(182, 342);
+            this.lb2.TabIndex = 3;
+            this.lb2.SelectedIndexChanged += new System.EventHandler(this.lb2_SelectedIndexChanged);
+            // 
+            // hozzaad1
+            // 
+            this.hozzaad1.Location = new System.Drawing.Point(222, 32);
+            this.hozzaad1.Name = "hozzaad1";
+            this.hozzaad1.Size = new System.Drawing.Size(75, 23);
+            this.hozzaad1.TabIndex = 4;
+            this.hozzaad1.Text = "Hozzáad";
+            this.hozzaad1.UseVisualStyleBackColor = true;
+            this.hozzaad1.Click += new System.EventHandler(this.hozzaad1_Click);
+            // 
+            // hozzaad2
+            // 
+            this.hozzaad2.Location = new System.Drawing.Point(607, 32);
+            this.hozzaad2.Name = "hozzaad2";
+            this.hozzaad2.Size = new System.Drawing.Size(75, 23);
+            this.hozzaad2.TabIndex = 5;
+            this.hozzaad2.Text = "Hozzáad";
+            this.hozzaad2.UseVisualStyleBackColor = true;
+            this.hozzaad2.Click += new System.EventHandler(this.hozzaad2_Click);
+            // 
+            // felulre1
+            // 
+            this.felulre1.Location = new System.Drawing.Point(222, 136);
+            this.felulre1.Name = "felulre1";
+            this.felulre1.Size = new System.Drawing.Size(75, 23);
+            this.felulre1.TabIndex = 6;
+            this.felulre1.Text = "Felülre";
+            this.felulre1.UseVisualStyleBackColor = true;
+            this.felulre1.Click += new System.EventHandler(this.felulre1_Click);
+            // 
+            // fel1
+            // 
+            this.fel1.Location = new System.Drawing.Point(222, 165);
+            this.fel1.Name = "fel1";
+            this.fel1.Size = new System.Drawing.Size(75, 23);
+            this.fel1.TabIndex = 7;
+            this.fel1.Text = "Fel";
+            this.fel1.UseVisualStyleBackColor = true;
+            this.fel1.Click += new System.EventHandler(this.fel1_Click);
+            // 
+            // le1
+            // 
+            this.le1.Location = new System.Drawing.Point(222, 194);
+            this.le1.Name = "le1";
+            this.le1.Size = new System.Drawing.Size(75, 23);
+            this.le1.TabIndex = 8;
+            this.le1.Text = "Le";
+            this.le1.UseVisualStyleBackColor = true;
+            this.le1.Click += new System.EventHandler(this.le1_Click);
+            // 
+            // alulra1
+            // 
+            this.alulra1.Location = new System.Drawing.Point(222, 223);
+            this.alulra1.Name = "alulra1";
+            this.alulra1.Size = new System.Drawing.Size(75, 23);
+            this.alulra1.TabIndex = 9;
+            this.alulra1.Text = "Alulra";
+            this.alulra1.UseVisualStyleBackColor = true;
+            this.alulra1.Click += new System.EventHandler(this.alulra1_Click);
+            // 
+            // alulra2
+            // 
+            this.alulra2.Location = new System.Drawing.Point(607, 223);
+            this.alulra2.Name = "alulra2";
+            this.alulra2.Size = new System.Drawing.Size(75, 23);
+            this.alulra2.TabIndex = 13;
+            this.alulra2.Text = "Alulra";
+            this.alulra2.UseVisualStyleBackColor = true;
+            this.alulra2.Click += new System.EventHandler(this.alulra2_Click);
+            // 
+            // le2
+            // 
+            this.le2.Location = new System.Drawing.Point(607, 194);
+            this.le2.Name = "le2";
+            this.le2.Size = new System.Drawing.Size(75, 23);
+            this.le2.TabIndex = 12;
+            this.le2.Text = "Le";
+            this.le2.UseVisualStyleBackColor = true;
+            this.le2.Click += new System.EventHandler(this.le2_Click);
+            // 
+            // fel2
+            // 
+            this.fel2.Location = new System.Drawing.Point(607, 165);
+            this.fel2.Name = "fel2";
+            this.fel2.Size = new System.Drawing.Size(75, 23);
+            this.fel2.TabIndex = 11;
+            this.fel2.Text = "Fel";
+            this.fel2.UseVisualStyleBackColor = true;
+            this.fel2.Click += new System.EventHandler(this.fel2_Click);
+            // 
+            // felulre2
+            // 
+            this.felulre2.Location = new System.Drawing.Point(607, 136);
+            this.felulre2.Name = "felulre2";
+            this.felulre2.Size = new System.Drawing.Size(75, 23);
+            this.felulre2.TabIndex = 10;
+            this.felulre2.Text = "Felülre";
+            this.felulre2.UseVisualStyleBackColor = true;
+            this.felulre2.Click += new System.EventHandler(this.felulre2_Click);
+            // 
+            // balraKuld
+            // 
+            this.balraKuld.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.balraKuld.Location = new System.Drawing.Point(342, 149);
+            this.balraKuld.Name = "balraKuld";
+            this.balraKuld.Size = new System.Drawing.Size(40, 30);
+            this.balraKuld.TabIndex = 14;
+            this.balraKuld.Text = "<";
+            this.balraKuld.UseVisualStyleBackColor = true;
+            this.balraKuld.Click += new System.EventHandler(this.balraKuld_Click);
+            // 
+            // jobraKuld
+            // 
+            this.jobraKuld.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jobraKuld.Location = new System.Drawing.Point(342, 185);
+            this.jobraKuld.Name = "jobraKuld";
+            this.jobraKuld.Size = new System.Drawing.Size(40, 32);
+            this.jobraKuld.TabIndex = 15;
+            this.jobraKuld.Text = ">";
+            this.jobraKuld.UseVisualStyleBackColor = true;
+            this.jobraKuld.Click += new System.EventHandler(this.jobraKuld_Click);
+            // 
+            // torol1
+            // 
+            this.torol1.Location = new System.Drawing.Point(222, 372);
+            this.torol1.Name = "torol1";
+            this.torol1.Size = new System.Drawing.Size(75, 23);
+            this.torol1.TabIndex = 16;
+            this.torol1.Text = "Töröl";
+            this.torol1.UseVisualStyleBackColor = true;
+            this.torol1.Click += new System.EventHandler(this.torol1_Click);
+            // 
+            // torol2
+            // 
+            this.torol2.Location = new System.Drawing.Point(607, 372);
+            this.torol2.Name = "torol2";
+            this.torol2.Size = new System.Drawing.Size(75, 23);
+            this.torol2.TabIndex = 17;
+            this.torol2.Text = "Töröl";
+            this.torol2.UseVisualStyleBackColor = true;
+            this.torol2.Click += new System.EventHandler(this.torol2_Click);
+            // 
+            // listak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 454);
+            this.Controls.Add(this.torol2);
+            this.Controls.Add(this.torol1);
+            this.Controls.Add(this.jobraKuld);
+            this.Controls.Add(this.balraKuld);
+            this.Controls.Add(this.alulra2);
+            this.Controls.Add(this.le2);
+            this.Controls.Add(this.fel2);
+            this.Controls.Add(this.felulre2);
+            this.Controls.Add(this.alulra1);
+            this.Controls.Add(this.le1);
+            this.Controls.Add(this.fel1);
+            this.Controls.Add(this.felulre1);
+            this.Controls.Add(this.hozzaad2);
+            this.Controls.Add(this.hozzaad1);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Name = "listak";
+            this.Text = "Listák";
+            this.Load += new System.EventHandler(this.listak_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.ListBox lb2;
+        private System.Windows.Forms.Button hozzaad1;
+        private System.Windows.Forms.Button hozzaad2;
+        private System.Windows.Forms.Button felulre1;
+        private System.Windows.Forms.Button fel1;
+        private System.Windows.Forms.Button le1;
+        private System.Windows.Forms.Button alulra1;
+        private System.Windows.Forms.Button alulra2;
+        private System.Windows.Forms.Button le2;
+        private System.Windows.Forms.Button fel2;
+        private System.Windows.Forms.Button felulre2;
+        private System.Windows.Forms.Button balraKuld;
+        private System.Windows.Forms.Button jobraKuld;
+        private System.Windows.Forms.Button torol1;
+        private System.Windows.Forms.Button torol2;
+    }
+}
+
---
+++ aafBeadando/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aafBeadando
+{
+    public partial class listak : Form
+    {
+        public listak()
+        {
+            InitializeComponent();
+        }
+
+        private void listak_Load(object sender, EventArgs e)
+        {
+            torol1.Enabled = false;
+            torol2.Enabled = false;
+            fel1.Enabled = false;
+            fel2.Enabled = false;
+            felulre1.Enabled = false;
+            felulre2.Enabled = false;
+            le1.Enabled = false;
+            le2.Enabled = false;
+            alulra1.Enabled = false;
+            alulra2.Enabled = false;
+            jobraKuld.Enabled = false;
+            balraKuld.Enabled = false;
+        }
+
+        public void addNewItem(int szam)
+        {
+            if (szam == 1)
+            {
+                if (text1.Text != "")
+                {
+                    string szoveg = text1.Text;
+                    lb1.Items.Add(szoveg);
+                }
+            }
+            else
+            {
+                if (text2.Text != "")
+                {
+                    string szoveg = text2.Text;
+                    lb2.Items.Add(szoveg);
+                }
+            }
+        }
+
+        public void removeItem(int szam)
+        {
+            if (szam == 1)
+            {
+                if (lb1.SelectedIndex != -1)
+                {
+                    lb1.Items.RemoveAt(lb1.SelectedIndex);
+                }
+            }
+            else
+            {
+                if (lb2.SelectedIndex != -1)
+                {
+                    lb2.Items.RemoveAt(lb2.SelectedIndex);
+                }
+            }
+        }
+
+        private void hozzaad1_Click(object sender, EventArgs e)
+        {
+            if (text1.Text != "")
+            {
+                addNewItem(1);
+            }
+        }
+
+        private void hozzaad2_Click(object sender, EventArgs e)
+        {
+            if (text2.Text != "")
+            {
+                addNewItem(2);
+            }
+
+        }
+
+        private void torol1_Click(object sender, EventArgs e)
+        {
+            removeItem(1);
+            torol1.Enabled = false;
+            fel1.Enabled = false;
+            felulre1.Enabled = false;
+            le1.Enabled = false;
+            alulra1.Enabled = false;
+        }
+
+        private void torol2_Click(object sender, EventArgs e)
+        {
+            removeItem(2);
+            torol2.Enabled = false;
+            fel2.Enabled = false;
+            felulre2.Enabled = false;
+            le2.Enabled = false;
+            alulra2.Enabled = false;
+        }
+
+        private void lb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            torol1.Enabled = true;
+            jobraKuld.Enabled = true;
+
+            if (lb1.SelectedIndex != 0)
+            {
+                fel1.Enabled = true;
+                felulre1.Enabled = true;
+            }
+            else
+            {
+                fel1.Enabled = false;
+                felulre1.Enabled = false;
+            }
+            if (lb1.SelectedIndex == lb1.Items.Count-1)
+            {
+                le1.Enabled = false;
+                alulra1.Enabled = false;
+            }
+            else
+            {
+                le1.Enabled = true;
+                alulra1.Enabled = true;
+            }
+
+        }
+
+        private void lb2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            torol2.Enabled = true;
+            balraKuld.Enabled = true;
+
+            if (lb2.SelectedIndex != 0)
+            {
+                fel2.Enabled = true;
+                felulre2.Enabled = true;
+            }
+            else
+            {
+                fel2.Enabled = false;
+                felulre2.Enabled = false;
+            }
+            if (lb2.SelectedIndex == lb2.Items.Count - 1)
+            {
+                le2.Enabled = false;
+                alulra2.Enabled = false;
+            }
+            else
+            {
+                le2.Enabled = true;
+                alulra2.Enabled = true;
+            }
+        }
+
+        private void felulre1_Click(object sender, EventArgs e)
+        {
+            string sv = lb1.Items[lb1.SelectedIndex].ToString();
+            lb1.Items[lb1.SelectedIndex] = lb1.Items[0];
+            lb1.Items[0] = sv;
+        }
+
+        private void felulre2_Click(object sender, EventArgs e)
+        {
+            string sv = lb2.Items[lb2.SelectedIndex].ToString();
+            lb2.Items[lb2.SelectedIndex] = lb2.Items[0];
+            lb2.Items[0] = sv;
+        }
+
+        private void fel1_Click(object sender, EventArgs e)
+        {
+            string sv = lb1.Items[lb1.SelectedIndex].ToString();
+            lb1.Items[lb1.SelectedIndex] = lb1.Items[lb1.SelectedIndex-1];
+            lb1.Items[lb1.SelectedIndex - 1] = sv;
+        }
+
+        private void fel2_Click(object sender, EventArgs e)
+        {
+            string sv = lb2.Items[lb2.SelectedIndex].ToString();
+            lb2.Items[lb2.SelectedIndex] = lb2.Items[lb2.SelectedIndex - 1];
+            lb2.Items[lb2.SelectedIndex - 1] = sv;
+        }
+
+        private void le1_Click(object sender, EventArgs e)
+        {
+            string sv = lb1.Items[lb1.SelectedIndex].ToString();
+            lb1.Items[lb1.SelectedIndex] = lb1.Items[lb1.SelectedIndex + 1];
+            lb1.Items[lb1.SelectedIndex + 1] = sv;
+        }
+
+        private void le2_Click(object sender, EventArgs e)
+        {
+            string sv = lb2.Items[lb2.SelectedIndex].ToString();
+            lb2.Items[lb2.SelectedIndex] = lb2.Items[lb2.SelectedIndex + 1];
+            lb2.Items[lb2.SelectedIndex + 1] = sv;
+        }
+
+        private void alulra1_Click(object sender, EventArgs e)
+        {
+            string sv = lb1.Items[lb1.SelectedIndex].ToString();
+            lb1.Items[lb1.SelectedIndex] = lb1.Items[lb1.Items.Count-1];
+            lb1.Items[lb1.Items.Count-1] = sv;
+        }
+
+        private void alulra2_Click(object sender, EventArgs e)
+        {
+            string sv = lb2.Items[lb2.SelectedIndex].ToString();
+            lb2.Items[lb2.SelectedIndex] = lb2.Items[lb2.Items.Count - 1];
+            lb2.Items[lb2.Items.Count - 1] = sv;
+        }
+
+        private void balraKuld_Click(object sender, EventArgs e)
+        {
+            string text = lb2.Items[lb2.SelectedIndex].ToString();
+            lb2.Items.RemoveAt(lb2.SelectedIndex);
+            lb1.Items.Add(text);
+        }
+
+        private void jobraKuld_Click(object sender, EventArgs e)
+        {
+            string text = lb1.Items[lb1.SelectedIndex].ToString();
+            lb1.Items.RemoveAt(lb1.SelectedIndex);
+            lb2.Items.Add(text);
+        }
+    }
+}
